8cbe03dd93946f008842680614567314339a93b4
Dania <detienne20@ufl.edu>
2020-04-03 19:45:19 -0400

    Login attempts

--- src/api_methods/shared_base.js
+++ src/api_methods/shared_base.js
@@ -1,4 +1,4 @@
-export const backendEndpoint = 'https://9382f522.ngrok.io/'; //http://localhost:3000 http://10.0.2.2:3000/
+export const backendEndpoint = 'https://52be4eaf.ngrok.io/'; //http://localhost:3000 http://10.0.2.2:3000/
 
 export const LOGIN_URL = 'users/login';
 export const REGISTER_URL = 'users/signup';
