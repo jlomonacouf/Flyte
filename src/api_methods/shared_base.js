a83e69a0dd11ec02a9196cfc7975a13d3ecc4143
jlomonacouf <jlomonaco@ufl.edu>
2020-04-04 03:44:48 -0400

    Changed Bcrypt code, login still broken

--- src/api_methods/shared_base.js
+++ src/api_methods/shared_base.js
@@ -1,4 +1,5 @@
-export const backendEndpoint = 'https://52be4eaf.ngrok.io/'; //http://localhost:3000 http://10.0.2.2:3000/
+export const backendEndpoint = 'https://4d0dea39.ngrok.io/'; //Note to self: MAKE SURE TO PUT A / AFTER THE NGROK URL http://localhost:3000
 
 export const LOGIN_URL = 'users/login';
 export const REGISTER_URL = 'users/signup';
+export const GET_SALT_URL = 'users/get-salt';
