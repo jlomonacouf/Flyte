9fa8d9e89d227a0d7db501751dea7b0cd893817d
jlomonacouf <jlomonaco@ufl.edu>
2020-04-05 03:22:38 -0400

    Worked on connecting itinerary creation to backend

--- src/api_methods/shared_base.js
+++ src/api_methods/shared_base.js
@@ -2,3 +2,7 @@ export const backendEndpoint = 'https://c35f93cd.ngrok.io/'; //Note to self: MAK
 
 export const LOGIN_URL = 'users/login';
 export const REGISTER_URL = 'users/signup';
+export const CREATE_ITINERARY_URL = 'itineraries/create-trip';
+export const DELETE_ITINERARY_URL = 'itineraries/delete-trip';
+export const CREATE_TRIP_URL = 'trips/create-trip';
+export const DELETE_TRIP_URL = 'trips/delete-trip';
